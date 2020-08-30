9f2059aed8c154e92dce3321e7a4aac9372f2ad6
TimRotich <rotich.tim1@gmail.com>
2020-08-30 11:13:31 +0300

    updating

--- app/src/main/java/com/example/cpmishealth/MainActivity.java
+++ app/src/main/java/com/example/cpmishealth/MainActivity.java
@@ -13,14 +13,22 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
-Button btnsignin;
-EditText username,pswrd;
+Button Login;
+
+    private EditText emailid;
+    private EditText pass;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,56 +37,117 @@ EditText username,pswrd;
 
 
 
-        btnsignin = (Button) findViewById(R.id.btnsignin);
-        username = (EditText) findViewById(R.id.username);
-        pswrd = (EditText) findViewById(R.id.pswrd);
+        emailid = findViewById(R.id.txtmail);
+        pass = findViewById(R.id.txtpassword);
+        Login = findViewById(R.id.btnsignin);
 
-        final FirebaseFirestore firebaseFirestore;
-        final DocumentReference db;
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        db = firebaseFirestore.collection("client").document();
 
-//        btnsignin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent= new Intent(MainActivity.this,dashboard.class);
-//                startActivity(intent);
-//            }
-//        });
+        mAuth = FirebaseAuth.getInstance();
 
-        btnsignin.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+
             @Override
-            public void onClick(View v) {
-                switch (v.getId()){
-                    case  R.id.btnsignin:
-                        if (username.getText().toString().equals("")){
-                            Toast.makeText(MainActivity.this, "Please enter valid username", Toast.LENGTH_SHORT).show();
-                        }else  if (pswrd.getText().toString().equals("")) {
-                            Toast.makeText(MainActivity.this, "Please enter correct password",Toast.LENGTH_SHORT).show();
-                        }
-                        db.collection("Admins")
-                                .get()
-                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        for (QueryDocumentSnapshot doc: task.getResult()) {
-                                            String a1 = username.getText().toString().trim();
-                                            String b1 = pswrd.getText().toString().trim();
-                                            if (username.getText().toString().equals(a1)& pswrd.getText().toString().equals(b1)) {
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mfirebaseuser = mAuth.getCurrentUser();
+                if (mfirebaseuser != null) {
 
-                                                Intent home = new Intent(MainActivity.this, dashboard.class);
-                                                startActivity(home);
-                                                Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
-                                                break;
-                                            }else {
-                                                Toast.makeText(MainActivity.this, "Cannot login, Incorrect credentials", Toast.LENGTH_SHORT).show();
-                                            }
+                    Toast.makeText(MainActivity.this, "Your are now Logged In.", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, dashboard.class);
 
-                                        }
-                                    }
-                                });
+                    startActivity(i);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please Login.", Toast.LENGTH_SHORT).show();
                 }
             }
+        };
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String txtmail = emailid.getText().toString();
+                String txtpassword = pass.getText().toString();
+
+
+                if (txtmail.isEmpty()) {
+
+                    emailid.setError("Email is Required");
+                    emailid.requestFocus();
+                } else if (txtpassword.isEmpty()) {
+
+                    pass.setError("Password is Required");
+                    pass.requestFocus();
+                } else if ((txtmail.isEmpty() && txtpassword.isEmpty())) {
+
+                    Toast.makeText(MainActivity.this, "Fields are Empty", Toast.LENGTH_SHORT).show();
+                } else if (txtmail != null && txtpassword != null) {
+
+
+                    mAuth.signInWithEmailAndPassword(txtmail, txtpassword).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Login Error,Please Login Again", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Intent intoclick = new Intent(MainActivity.this, dashboard.class);
+                                startActivity(intoclick);
+                            }
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+
         });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAuth.signOut();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
     }
 }
